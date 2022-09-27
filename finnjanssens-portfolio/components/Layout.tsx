0631f029fb711faeadb3603e862c941bab7b7490
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-09-27 10:16:02 +0200

    Add nav and basic styling

---
+++ finnjanssens-portfolio/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "./Nav";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Finn Janssens | Portfolio</title>
+      </Head>
+      <Nav />
+      <main className="px-4">
+        <div className="flex justify-center items-center mx-auto w-2/4 rounded-lg my-16 p-16">
+          <div className="text-2xl font-medium">{children}</div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ finnjanssens-portfolio/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1 className="font-bold text-5xl">Finn Janssens</h1>
+    </Layout>
+  );
+}
