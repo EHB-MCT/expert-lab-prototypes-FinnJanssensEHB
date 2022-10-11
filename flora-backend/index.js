7c22e11a3d7f9b4d5b5c1ae9a11e1b60068396bf
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-10-11 12:59:57 +0200

    Basic api with pagination

--- flora-backend/index.js
+++ flora-backend/index.js
@@ -1,32 +1,37 @@
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const express = require("express");
+const app = express();
+const port = 3000;
 
-// Connection URL
+const { MongoClient } = require("mongodb");
+
 const url = process.env.MONGODB_URI;
 const client = new MongoClient(url);
 
-// Database Name
 const dbName = process.env.MONGODB_DATABASE;
 
 async function findOccurences(skip, limit) {
-  // Use connect method to connect to the server
   await client.connect();
   console.log("Connected successfully to server");
   const db = client.db(dbName);
   const collection = db.collection(process.env.MONGODB_COLLECTION);
 
-  // define an empty query document
   const query = {};
-  // sort in descending (-1) order by length
   const cursor = collection.find(query).limit(limit).skip(skip);
-  await cursor.forEach((i) => console.log(i.recordedBy));
-
-  return "done.";
+  const results = await cursor.toArray();
+  console.log("results 1", results);
+  return results;
 }
 
-findOccurences(0, 15)
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+app.get("/occurences", async (req, res) => {
+  const page = parseInt(req.query.page) || 0;
+  const pageSize = parseInt(req.query.pageSize) || 10;
+  await findOccurences(page, pageSize)
+    .then((data) => res.json({ page: page, pageSize: pageSize, results: data }))
+    .catch(console.error)
+    .finally(() => client.close());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
