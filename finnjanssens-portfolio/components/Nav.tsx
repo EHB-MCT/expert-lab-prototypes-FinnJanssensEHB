002db78ba1239239b117c72baa008a9bd4fe7c19
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-10-19 11:46:22 +0200

    Add shifting of nav

--- finnjanssens-portfolio/components/Nav.tsx
+++ finnjanssens-portfolio/components/Nav.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export default function Nav() {
+  const [navItems, setNavItems] = useState([
+    "Home",
+    "Projects",
+    "Photography",
+    "Contact",
+  ]);
+
+  function handleNav(index: number) {
+    let a = navItems.slice(index);
+    let b = navItems.slice(0, index);
+    console.log(a.concat(b));
+    setNavItems(a.concat(b));
+  }
   return (
     <>
       <nav className="col-span-4 mt-4">
         <div className="grid grid-cols-4 gap-x-5">
-          <h3 className="border-l-2 border-black pl-2 mb-4 active">Home</h3>
+          {navItems.map((item, index) => {
+            let style = "";
+            index == 0
+              ? (style = "border-l-2 border-black pl-2 mb-4 active")
+              : (style = "border-l-2 border-black pl-2 mb-4");
+            return (
+              <h3
+                key={index}
+                className={style}
+                onClick={() => {
+                  handleNav(index);
+                }}
+              >
+                {item}
+              </h3>
+            );
+          })}
+          {/* <h3 className="border-l-2 border-black pl-2 mb-4 active">Home</h3>
           <h3 className="border-l-2 border-black pl-2 mb-4">Projects</h3>
           <h3 className="border-l-2 border-black pl-2 mb-4">Photography</h3>
-          <h3 className="border-l-2 border-black pl-2 mb-4">Contact</h3>
+          <h3 className="border-l-2 border-black pl-2 mb-4">Contact</h3> */}
         </div>
       </nav>
     </>
